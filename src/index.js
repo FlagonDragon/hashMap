f401229df96a9d668a29892b8496f44f20804981
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2025-11-21 06:40:11 -0300

    add hash function

--- src/index.js
+++ src/index.js
@@ -2,3 +2,30 @@ import './styles.css';
 
 console.log('woooorrrkkk time!!!');
 
+class hashMap {
+
+    constructor(loadFactor, capacity) {
+        this.loadFactor = loadFactor;
+        this.capacity = capacity;
+    }
+
+    hash(key) {
+
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+
+        return hashCode;
+
+    }
+
+};
+
+const hashBrown = new hashMap(0.75, 16);
+
+console.log(hashBrown.loadFactor+' '+hashBrown.capacity);
+
+console.log(hashBrown.hash('Sara'));
