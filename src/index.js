9f1dc5061fcdde3aaaf298bcfcdafe66ebd06d4b
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2025-11-22 05:12:09 -0300

    add set function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles.css';
+import linkedList from "./linkedList";
 
 console.log('woooorrrkkk time!!!');
 
@@ -7,6 +8,7 @@ class hashMap {
     constructor(loadFactor, capacity) {
         this.loadFactor = loadFactor;
         this.capacity = capacity;
+        this.map = [];
     }
 
     hash(key) {
@@ -15,11 +17,58 @@ class hashMap {
 
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
-            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
         }
 
         return hashCode;
 
+    }
+
+    set(key, value) {
+
+        let hashCode = this.hash(key);
+
+        if (!this.map[hashCode]) {
+            this.map[hashCode] = new linkedList('bucket '+hashCode);
+
+            this.map[hashCode].append({key: key, value: value});
+
+            return;
+
+        } else {
+
+            this.map[hashCode].pointer = this.map[hashCode].head
+
+            while (this.map[hashCode].pointer.nextNode != null || this.map[hashCode].pointer == this.map[hashCode].head){
+
+                if (this.map[hashCode].pointer.value.key == key) {
+
+                    let storeIndex = this.map[hashCode].finds(this.map[hashCode].pointer.value.value);
+                    
+                    console.log(storeIndex);
+
+                    this.map[hashCode].removeAt(storeIndex);
+
+                    this.map[hashCode].insertAt({key: key, value: value}, storeIndex);
+
+                    return;
+
+                }
+
+                if (this.map[hashCode].pointer.nextNode != null) {
+
+                    this.map[hashCode].pointer = this.map[hashCode].pointer.nextNode;
+
+                }
+                
+            }
+
+            this.map[hashCode].append({key: key, value: value});
+        }
+
+        
+        console.log(this.map[hashCode]);
+        
     }
 
 };
@@ -28,4 +77,21 @@ const hashBrown = new hashMap(0.75, 16);
 
 console.log(hashBrown.loadFactor+' '+hashBrown.capacity);
 
-console.log(hashBrown.hash('Sara'));
+// console.log(hashBrown.hash('Sara'));
+// console.log(hashBrown.hash('Sara'));
+
+hashBrown.set('King','Stannis');
+hashBrown.set('King','Renly');
+hashBrown.set('King','Robert');
+
+
+
+console.log(hashBrown.map);
+// console.log(hashBrown.map[7].head.value.value);
+// console.log(hashBrown.map[7].size());
+console.log(hashBrown.map[7].toStringMap());
+console.log(hashBrown.map[7].head);
+console.log(hashBrown.map[7].head.nextNode);
+
+
+
---
+++ src/linkedList.js
@@ -0,0 +1,310 @@
+class node {
+
+    constructor(value = null, nextNode = null) {
+
+        this.value = value
+        this.nextNode = nextNode
+
+    }
+};
+
+class linkedList {
+
+    constructor(title, pointer = null) {
+
+        this.title = title
+        this.pointer = pointer
+        this.head;
+
+    }
+
+    append(value) {
+
+        console.log('append value: '+value);
+
+        if (!this.head) {
+
+            console.log('Empty list. Creating head');
+        
+            this.head = new node(value);
+
+            this.pointer = this.head;
+
+        } else if (this.pointer.nextNode == null) {
+
+            this.pointer.nextNode = new node(value);
+
+            this.pointer = this.pointer.nextNode;
+
+        } else {
+
+            while (this.pointer.nextNode != null) {
+
+                this.pointer = this.pointer.nextNode;
+
+            }
+
+            this.pointer.nextNode = new node(value);
+
+            this.pointer = this.pointer.nextNode;
+
+        }
+
+    };
+
+    prepend(value) {
+
+        console.log('prepend value: '+value);
+
+        if (!this.head) {
+
+            console.log('Empty list. Creating head');
+        
+            this.head = new node(value);
+
+            this.pointer = this.head;
+
+        } else {
+
+            this.head = new node(value, this.head);
+
+            this.pointer = this.head;
+
+        }
+
+    }
+
+    size() {
+
+        if (!this.head) {
+            return 0;
+        }
+        
+        this.pointer = this.head;
+        
+        console.log(this.pointer);
+
+        let nodeCount = 1; 
+
+        while (this.pointer.nextNode != null) {
+
+            console.log(this.pointer.nextNode);
+
+            this.pointer = this.pointer.nextNode;
+
+            nodeCount += 1;
+
+        }
+
+        return nodeCount;
+
+    }
+
+    returnHead() {
+        return this.head;
+    }
+
+    tail() {
+
+        if (!this.head) {
+            return 'Empty list';
+        }
+
+        while (this.pointer.nextNode != null) {
+
+            console.log(this.pointer.nextNode);
+
+            this.pointer = this.pointer.nextNode;
+
+        }
+
+        return this.pointer;
+        
+    }
+
+    at(index) {
+
+        if (index > this.size() || index < 1) {
+            return "Node doesn't exist";
+        }
+
+        if (this.head.nextNode == null) {
+            return this.head;
+        }
+
+        this.pointer = this.head;
+
+        for (let i = 1; i < index; i++) {
+                        
+            this.pointer = this.pointer.nextNode;
+
+        }
+
+        return this.pointer;
+
+    }
+
+    pop() {
+        
+        this.pointer = this.at(this.size() - 1);
+
+        this.pointer.nextNode = null;
+
+        return 'Tail popped!'
+         
+    }
+
+    contains(value) {
+
+        if (!this.head) {
+            return false;
+        }
+
+        this.pointer = this.head;
+        
+        while (this.pointer != null) {
+            
+            if (this.pointer.value == value) {
+                return true
+            }
+
+            this.pointer = this.pointer.nextNode;
+
+        }
+
+        return false
+
+    }
+
+    finds(value) {
+
+        if (!this.head) {
+            return 'Value not found';
+        }
+
+        const length = this.size()
+        
+        this.pointer = this.head
+        
+        for (let i = 1; i <= length; i++) {
+            
+            if (this.pointer.value.value == value) {
+                return i;
+            }
+                
+            this.pointer = this.pointer.nextNode;
+
+        }
+
+        return 'Value not found';
+
+    }
+
+    toString() {
+
+        if (!this.head) {
+            return 'Empty list';
+        }
+        
+        this.pointer = this.head
+
+        let myString = '';
+        
+        while (this.pointer.nextNode != null) {
+          
+            myString += `( ${this.pointer.value} ) -> `
+                
+            this.pointer = this.pointer.nextNode;
+
+        }
+
+        myString += `( ${this.pointer.value} ) -> ${this.pointer.nextNode}`
+
+        return myString;
+
+    }
+
+    toStringMap() {
+
+        if (!this.head) {
+            return 'Empty list';
+        }
+        
+        this.pointer = this.head
+
+        let myString = '';
+        
+        while (this.pointer.nextNode != null) {
+          
+            myString += `( ${this.pointer.value.value} ) -> `
+                
+            this.pointer = this.pointer.nextNode;
+
+        }
+
+        myString += `( ${this.pointer.value.value} ) -> ${this.pointer.nextNode}`
+
+        return myString;
+
+    }
+
+    insertAt(value, index) {
+
+        const length = this.size()
+
+        console.log(length);
+        
+        if (index < 1 || index > length + 1) {
+            return 'Invalid index!'
+        }
+
+        if (index == 1) {
+
+            this.prepend(value);
+
+        } else if (index == length) {
+
+            this.at(index - 1).nextNode = new node(value, this.at(index));
+
+        } else if (index == length + 1) {
+
+            this.append(value);
+
+        } else {
+
+        this.at(index - 1).nextNode = new node(value, this.at(index));
+
+        }
+
+        return 'value added at node '+index+': ' +value;
+
+    }
+
+    removeAt(index) {
+
+        const length = this.size()
+        
+        if (index < 1 || index > length) {
+            return 'Invalid index!'
+        }
+
+        if (length == 1) {
+            this.head = null;
+            this.pointer = null;
+            return 'Head node was removed';
+        }
+
+        this.at(index - 1).nextNode = this.at(index - 1).nextNode.nextNode;
+
+        return 'Node '+index+' was removed';
+
+    }
+
+};
+
+export default linkedList;
+
+
+
+
+
