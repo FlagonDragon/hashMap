cfb0b464c901d5fa7108311cd39a9a9e837d4cf3
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2025-11-24 08:09:06 -0300

    add has and remove functions

--- src/index.js
+++ src/index.js
@@ -28,25 +28,24 @@ class hashMap {
 
         let hashCode = this.hash(key);
 
+        if (hashCode < 0 || hashCode >= this.capacity) {
+            throw new Error("Trying to access index out of bounds");
+        }       
+
         if (!this.map[hashCode]) {
+
             this.map[hashCode] = new linkedList('bucket '+hashCode);
-
-            this.map[hashCode].append({key: key, value: value});
-
-            return;
 
         } else {
 
             this.map[hashCode].pointer = this.map[hashCode].head
 
-            while (this.map[hashCode].pointer.nextNode != null || this.map[hashCode].pointer == this.map[hashCode].head){
+            while (this.map[hashCode].pointer.nextNode != null || this.map[hashCode].pointer == this.map[hashCode].head) {
 
                 if (this.map[hashCode].pointer.value.key == key) {
 
                     let storeIndex = this.map[hashCode].finds(this.map[hashCode].pointer.value.value);
                     
-                    console.log(storeIndex);
-
                     this.map[hashCode].removeAt(storeIndex);
 
                     this.map[hashCode].insertAt({key: key, value: value}, storeIndex);
@@ -55,20 +54,136 @@ class hashMap {
 
                 }
 
-                if (this.map[hashCode].pointer.nextNode != null) {
+                this.map[hashCode].pointer = this.map[hashCode].pointer.nextNode;
 
-                    this.map[hashCode].pointer = this.map[hashCode].pointer.nextNode;
+                if (this.map[hashCode].pointer == null) {
+
+                    break;
 
                 }
                 
             }
 
-            this.map[hashCode].append({key: key, value: value});
         }
 
+        this.map[hashCode].append({key: key, value: value});
         
         console.log(this.map[hashCode]);
         
+    }
+
+    get(key) {
+        
+        for (let bucket of this.map) {
+
+            if (bucket) {
+
+                bucket.pointer = bucket.head;
+
+                while (bucket.pointer.nextNode != null || bucket.pointer == bucket.head) {
+
+                    if (bucket.pointer.value.key == key) {
+
+                        return bucket.pointer.value.value;
+
+                    }
+
+                    bucket.pointer = bucket.pointer.nextNode;
+
+                    if (bucket.pointer == null) {
+
+                        break;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        return null;
+
+    }
+
+    has(key) {
+        
+        for (let bucket of this.map) {
+
+            if (bucket) {
+
+                bucket.pointer = bucket.head;
+
+                while (bucket.pointer.nextNode != null || bucket.pointer == bucket.head) {
+
+                    if (bucket.pointer.value.key == key) {
+
+                        return true;
+
+                    }
+
+                    bucket.pointer = bucket.pointer.nextNode;
+
+                    if (bucket.pointer == null) {
+
+                        break;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        return false;
+
+    }
+
+    remove(key) {
+        
+        for (let bucket of this.map) {
+
+            if (bucket) {
+
+                bucket.pointer = bucket.head;
+
+                while (bucket.pointer.nextNode != null || bucket.pointer == bucket.head) {
+
+                    if (bucket.pointer.value.key == key) {
+
+                        let storeIndex = bucket.finds(bucket.pointer.value.value);
+
+                        console.log(storeIndex);
+                        
+                        bucket.removeAt(storeIndex);
+
+                        if (bucket.size() == 0) {
+                            console.log(this.map.indexOf(bucket));
+                            
+                            this.map[this.map.indexOf(bucket)] = undefined;
+                        }
+
+                        return true;
+
+                    }
+
+                    bucket.pointer = bucket.pointer.nextNode;
+
+                    if (bucket.pointer == null) {
+
+                        break;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        return false;
+
     }
 
 };
@@ -77,21 +192,27 @@ const hashBrown = new hashMap(0.75, 16);
 
 console.log(hashBrown.loadFactor+' '+hashBrown.capacity);
 
-// console.log(hashBrown.hash('Sara'));
-// console.log(hashBrown.hash('Sara'));
-
 hashBrown.set('King','Stannis');
-hashBrown.set('King','Renly');
-hashBrown.set('King','Robert');
+hashBrown.set('Hand','Renly');
+hashBrown.set('Former','Robert');
+
+console.log(hashBrown.get('King'));
+console.log(hashBrown.has('King'));
+console.log(hashBrown.has('Heir'));
+console.log(hashBrown.remove('King'));
+console.log(hashBrown.has('King'));
+// console.log(hashBrown.map[7].size());
+console.log(hashBrown.map[8]);
+
+console.log(!hashBrown.map[7]);
 
 
 
 console.log(hashBrown.map);
-// console.log(hashBrown.map[7].head.value.value);
-// console.log(hashBrown.map[7].size());
-console.log(hashBrown.map[7].toStringMap());
-console.log(hashBrown.map[7].head);
-console.log(hashBrown.map[7].head.nextNode);
+// console.log(hashBrown.map[7].toStringMap());
+
+
+
 
 
 
